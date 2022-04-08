e18ef1f65c74541c8ee41b60d119b5b912363e92
Juan Naveros <90868042+jnaveros40@users.noreply.github.com>
2022-04-08 15:36:23 -0500

    Add files via upload

--- client/gui_app_facturacion.py
+++ client/gui_app_facturacion.py
@@ -148,7 +148,17 @@ class Frame_facturacion (tk.Frame):
         self.mi_Valor = tk.StringVar()
         self.entry_Valor = tk.Entry(self, textvariable=self.mi_Valor)
         self.entry_Valor.config(width=8,font=('Times New Roman',18,"bold"))
-        self.entry_Valor.grid(row=7, column=4, padx=10, rowspan=3)                
+        self.entry_Valor.grid(row=7, column=4, padx=10, rowspan=3)
+
+        self.mi_Id_inquilino = tk.StringVar()
+        self.entry_Id_inquilino = tk.Entry(self, textvariable=self.mi_Id_inquilino)
+        self.entry_Id_inquilino.config (width=1)
+        self.entry_Id_inquilino.grid(row=0, column=7)
+
+        self.mi_Id_apartamento = tk.StringVar()
+        self.entry_Id_apartamento = tk.Entry(self, textvariable=self.mi_Id_apartamento)
+        self.entry_Id_apartamento.config(width=1)
+        self.entry_Id_apartamento.grid(row=1, column=7)                
 
         #CREACION DE LOS BOTONES
         self.boton_Nuevo = tk.Button(self, text='Nuevo', command= self.habilitar_campos)
@@ -171,8 +181,10 @@ class Frame_facturacion (tk.Frame):
 
     def habilitar_campos(self):
         #self.id_apartamento=None
+        self.entry_Id_inquilino.config(state='normal')
         self.entry_Nombre_de.config(state='normal')
         self.entry_Apellido_de.config(state='normal')
+        self.entry_Id_apartamento.config(state='normal')
         self.entry_Direccion.config(state='normal')
         self.entry_Numero_apartamento.config(state='normal')
         self.entry_Nombre_por.config(state='normal')
@@ -187,8 +199,10 @@ class Frame_facturacion (tk.Frame):
 
     def desabilitar_campos(self):
         self.ID_FACTURACION=None
+        self.mi_Id_inquilino.set('')
         self.mi_Nombre_de.set('')
         self.mi_Apellido_de.set('')
+        self.mi_Id_apartamento.set('')
         self.mi_Direccion.set('')
         self.mi_Numero_apartamento.set('')
         self.mi_Nombre_por.set('')
@@ -198,8 +212,10 @@ class Frame_facturacion (tk.Frame):
         self.mi_Fecha_final.set('')
         self.mi_Valor.set('')
          
+        self.entry_Id_inquilino.config(state='disabled')
         self.entry_Nombre_de.config(state='disabled')
         self.entry_Apellido_de.config(state='disabled')
+        self.entry_Id_apartamento.config(state='disabled')
         self.entry_Direccion.config(state='disabled')
         self.entry_Numero_apartamento.config(state='disabled')
         self.entry_Nombre_por.config(state='disabled')
@@ -214,6 +230,7 @@ class Frame_facturacion (tk.Frame):
 
     def guardar_datos(self):
         facturacion = Facturacion(
+            self.mi_Id_inquilino.get(),
             self.mi_Nombre_de.get(),
             self.mi_Apellido_de.get(),
             self.mi_Direccion.get(),
@@ -223,6 +240,7 @@ class Frame_facturacion (tk.Frame):
             self.mi_Fecha_actual.get(),
             self.mi_Fecha_inicio.get(),
             self.mi_Fecha_final.get(),
+            self.mi_Valor.get(),
             self.mi_Valor.get()
         )
         if self.ID_FACTURACION == None:
@@ -238,12 +256,14 @@ class Frame_facturacion (tk.Frame):
         self.lista_facturacion.reverse()
 
         self.tabla = ttk.Treeview(self, height=5,
-        column = ('NOMBRE_INQUILINO', 'APELLIDO_INQUILINO', 'DIRECCION', 'APARTAMENTO', 'NOMBRE_PROPIETARIO', 'APELLIDO_PROPIETARIO', 'FECHA', 'FECHA_INICIO', 'FECHA_FINAL', 'VALOR'))
+        column = ('ID_INQUILINO','NOMBRE_INQUILINO', 'APELLIDO_INQUILINO','ID_APARTAMENTO', 'DIRECCION', 'APARTAMENTO', 'NOMBRE_PROPIETARIO', 'APELLIDO_PROPIETARIO', 'FECHA', 'FECHA_INICIO', 'FECHA_FINAL', 'VALOR'))
         
         self.tabla.grid(row=12, column=0, columnspan=7, sticky='nse')
         self.tabla.column('#0', width=35)
+        self.tabla.column('ID_INQUILINO', width=25)
         self.tabla.column('NOMBRE_INQUILINO',width=120)
         self.tabla.column('APELLIDO_INQUILINO',width=120)
+        self.tabla.column('ID_APARTAMENTO', width=25)
         self.tabla.column('DIRECCION',width=120)
         self.tabla.column('APARTAMENTO',width=80)
         self.tabla.column('NOMBRE_PROPIETARIO',width=120)
@@ -259,21 +279,23 @@ class Frame_facturacion (tk.Frame):
         self.tabla.configure(yscrollcommand=self.scroll.set)
 
         self.tabla.heading('#0', text='ID')
-        self.tabla.heading('#1', text='NOMBRE_INQUILINO')
-        self.tabla.heading('#2', text='APELLIDO_INQUILINO')
-        self.tabla.heading('#3', text='DIRECCION')
-        self.tabla.heading('#4', text='APARTAMENTO')
-        self.tabla.heading('#5', text='NOMBRE_PROPIETARIO')
-        self.tabla.heading('#6', text='APELLIDO_PROPIETARIO')
-        self.tabla.heading('#7', text='FECHA')
-        self.tabla.heading('#8', text='FECHA_INICIO')
-        self.tabla.heading('#9', text='FECHA_FINAL')
-        self.tabla.heading('#10', text='VALOR')
+        self.tabla.heading('#1', text='ID INQUILINO')
+        self.tabla.heading('#2', text='NOMBRE INQUILINO')
+        self.tabla.heading('#3', text='APELLIDO INQUILINO')
+        self.tabla.heading('#4', text='ID APARTAMENTO')
+        self.tabla.heading('#5', text='DIRECCION')
+        self.tabla.heading('#6', text='APARTAMENTO')
+        self.tabla.heading('#7', text='NOMBRE PROPIETARIO')
+        self.tabla.heading('#8', text='APELLIDO PROPIETARIO')
+        self.tabla.heading('#9', text='FECHA')
+        self.tabla.heading('#10', text='FECHA INICIO')
+        self.tabla.heading('#11', text='FECHA FINAL')
+        self.tabla.heading('#12', text='VALOR')
         
-        #iterar la lista de peliculas
+        #iterar la lista de facturacion
         for a in self.lista_facturacion:
             self.tabla.insert('',0,text=a[0],
-            values=(a[1],a[2],a[3],a[4],a[5], a[6],a[7],a[8],a[9], a[10]))
+            values=(a[1],a[2],a[3],a[4],a[5],a[6],a[7],a[8],a[9],a[10],a[11],a[12]))
 
         #CREACION DE LOS BOTONES
         self.boton_Editar = tk.Button(self, text='Editar', command=self.editar_datos)
@@ -300,20 +322,24 @@ class Frame_facturacion (tk.Frame):
     def editar_datos(self):
         try:
             self.ID_FACTURACION = self.tabla.item(self.tabla.selection())['text']
-            self.nombre_de_facturacion = self.tabla.item(self.tabla.selection())['values'][0]
-            self.apellido_de_facturacion = self.tabla.item(self.tabla.selection())['values'][1]
-            self.direccion_facturacion = self.tabla.item(self.tabla.selection())['values'][2]
-            self.numero_apartamento_facturacion = self.tabla.item(self.tabla.selection())['values'][3]
-            self.nombre_por_facturacion = self.tabla.item(self.tabla.selection())['values'][4]
-            self.apellido_por_facturacion = self.tabla.item(self.tabla.selection())['values'][5]
-            self.fecha_actual_facturacion = self.tabla.item(self.tabla.selection())['values'][6]
-            self.fecha_inicio_facturacion = self.tabla.item(self.tabla.selection())['values'][7]
-            self.fecha_final_facturacion = self.tabla.item(self.tabla.selection())['values'][8]
-            self.valor_facturacion = self.tabla.item(self.tabla.selection())['values'][9]
+            self.id_inquilino = self.tabla.item(self.tabla.selection()['values'][0])
+            self.nombre_de_facturacion = self.tabla.item(self.tabla.selection())['values'][1]
+            self.apellido_de_facturacion = self.tabla.item(self.tabla.selection())['values'][2]
+            self.id_apartamento = self.tabla.item(self.tabla.selection()['values'][3])
+            self.direccion_facturacion = self.tabla.item(self.tabla.selection())['values'][4]
+            self.numero_apartamento_facturacion = self.tabla.item(self.tabla.selection())['values'][5]
+            self.nombre_por_facturacion = self.tabla.item(self.tabla.selection())['values'][6]
+            self.apellido_por_facturacion = self.tabla.item(self.tabla.selection())['values'][7]
+            self.fecha_actual_facturacion = self.tabla.item(self.tabla.selection())['values'][8]
+            self.fecha_inicio_facturacion = self.tabla.item(self.tabla.selection())['values'][9]
+            self.fecha_final_facturacion = self.tabla.item(self.tabla.selection())['values'][10]
+            self.valor_facturacion = self.tabla.item(self.tabla.selection())['values'][11]
 
             self.habilitar_campos()
+            self.entry_Id_inquilino.insert(0, self.id_inquilino)
             self.entry_Nombre_de.insert(0, self.nombre_de_facturacion)
             self.entry_Apellido_de.insert(0, self.apellido_de_facturacion)
+            self.entry_Id_apartamento.insert(0, self.id_apartamento)
             self.entry_Direccion.insert(0, self.direccion_facturacion)
             self.entry_Numero_apartamento.insert(0, self.numero_apartamento_facturacion)
             self.entry_Nombre_por.insert(0, self.nombre_por_facturacion)
@@ -378,20 +404,14 @@ class Frame_facturacion (tk.Frame):
                                                 cursor='hand2', activebackground='#35BD6F')
         self.boton_Editar.grid(row=28,column=0, padx=10,pady=10)
 
-        #CREACION DE LOS BOTONES
-        self.boton_Editar = tk.Button(self, text='Seleccionar', command=self.seleccionar_apartamento)
-        self.boton_Editar.config(width=20, font=('Times New Roman',12,'bold'),
-                                                fg = '#DAD5D6', bg='#158645',
-                                                cursor='hand2', activebackground='#35BD6F')
-        self.boton_Editar.grid(row=28,column=4, padx=10,pady=10)
-
     def seleccionar_inquilino(self):
         try:
-            self.ID_INQUILINO = self.tabla1.item(self.tabla1.selection())['text']
+            self.id_inquilino = self.tabla1.item(self.tabla1.selection())['text']
             self.nombre_inquilino = self.tabla1.item(self.tabla1.selection())['values'][0]
             self.apellidos_inquilino = self.tabla1.item(self.tabla1.selection())['values'][1]
                             
             self.habilitar_campos()
+            self.entry_Id_inquilino.insert(0, self.id_inquilino)
             self.entry_Nombre_de.insert(0, self.nombre_inquilino)
             self.entry_Apellido_de.insert(0,self.apellidos_inquilino)
                 
@@ -406,13 +426,13 @@ class Frame_facturacion (tk.Frame):
 
         self.tabla2 = ttk.Treeview(self, height=5, 
         column = ('Direccion', 'Numero_Apartamento'))
-        self.tabla2.grid(row=26, column=4, columnspan=4, sticky='nse')
+        self.tabla2.grid(row=26, column=2, columnspan=4, sticky='nse')
 
         self.tabla2.column('#0', width=35)
 
         self.scroll=ttk.Scrollbar(self,
         orient='vertical', command= self.tabla2.yview)
-        self.scroll.grid(row=26, column=8, sticky='nse')
+        self.scroll.grid(row=26, column=6, sticky='nse')
         self.tabla2.configure(yscrollcommand=self.scroll.set)
 
         self.tabla2.heading('#0', text='ID')
@@ -424,6 +444,12 @@ class Frame_facturacion (tk.Frame):
             self.tabla2.insert('',0,text=a[0],
             values=(a[1],a[2]))
 
+        #CREACION DE LOS BOTONES
+        self.boton_Editar = tk.Button(self, text='Seleccionar', command=self.seleccionar_apartamento)
+        self.boton_Editar.config(width=20, font=('Times New Roman',12,'bold'),
+                                                fg = '#DAD5D6', bg='#158645',
+                                                cursor='hand2', activebackground='#35BD6F')
+        self.boton_Editar.grid(row=28,column=4, padx=10,pady=10)    
 
     def seleccionar_apartamento(self):
         try:
@@ -432,6 +458,7 @@ class Frame_facturacion (tk.Frame):
             self.numero_apartamento_apartamento = self.tabla2.item(self.tabla2.selection())['values'][1]
                     
             self.habilitar_campos()
+            self.entry_Id_apartamento.insert(0, self.id_apartamento)
             self.entry_Direccion.insert(0, self.direccion_apartamento)
             self.entry_Numero_apartamento.insert(0,self.numero_apartamento_apartamento)
             
